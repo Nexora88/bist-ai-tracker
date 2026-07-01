a9ef2f8499039d9b83f8898d7706806304b88d74
Nexora88 <ahmeteymenbakrac@gmail.com>
2026-07-01 15:35:08 +0300

    Add global configuration system

---
+++ assets/js/config.js
@@ -0,0 +1,30 @@
+/*
+========================================
+BIST AI Tracker
+Global Config
+========================================
+*/
+
+const CONFIG = {
+
+    APP_NAME: "BIST AI Tracker",
+
+    VERSION: "2.0.0",
+
+    DEFAULT_SYMBOL: "AEFES",
+
+    REFRESH_INTERVAL: 10000,
+
+    MARKETS: {
+
+        BIST: ".IS",
+
+        NASDAQ: "",
+
+        NYSE: "",
+
+        CRYPTO: ""
+
+    }
+
+}
